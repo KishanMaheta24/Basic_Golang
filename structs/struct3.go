4b67ef56d55035c1e9b779834ddb3ad2df8031ed
Kishan Maheta <kishan.m@simformsolutions.com>
2023-04-27 15:45:26 +0530

    changes

--- structs/struct3.go
+++ structs/struct3.go
@@ -15,9 +15,19 @@ type Marks struct {
 	Chemistry int
 }
 
+type Student1 struct {
+	Name string
+	Age  int
+	Id   int
+	Marks
+}
+
 func main() {
 	marks := Marks{90, 80, 70}
 	v := Student{"Dummy", 2, 77, marks}
+	v1 := Student1{"Dummy", 2, 77, Marks{90, 80, 70}}
 	fmt.Println(v)
 	fmt.Println(v.Marks.Maths)
+	fmt.Println(v1.Maths)
+
 }
--- variables/variables.go
+++ variables/variables.go
@@ -1,10 +1,45 @@
 package main
 
+import "fmt"
+
 /*
+Variable names must begin with a letter or an underscore. A variable name can include a to z, A to Z, digits 1 to 9, and '_'.
+A variable cannot begin with a number.
+A variable name cannot use Go keywords as variable names.
+A variable name can only contain alphanumeric characters and underscore.
+A variable name cannot contain spaces.
+*/
 
+//declaration type 1
 
- */
+var num int = 20           // int variable
+var amount float32 = 49.99 // float variable
+var isValid bool = true    // bool variable
+var name string = "avbcx"  // string variable
+
+//declaration type 2
+
+//var num = 20
+//var amount = 49.99
+//var isValid = true
+//var name = "asdv"
+
+//declaration type 3
+
+//var (
+//num = 20
+//amount = 49.99
+// isValid = true
+// name = "asdv"
+//)
 
 func main() {
 
+	//declaration type 3 : shorthand declaration
+
+	//num := 20
+	//amount := 49.99
+	//isValid := true
+	//name := "fdg"
+	fmt.Println(num, amount, isValid, name)
 }
